f2a832ac6dd0a13173f8fd562d8298da52dd8e80
loziamina <lozi.amina.sio@gmail.com>
2023-10-17 09:50:56 +0200

    satisfait du code

--- game.js
+++ game.js
@@ -42,6 +42,8 @@ var mainState = {
         game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this); 
 
         // Rotate the bird    
+        if (this.bird.angle < 20)
+        this.bird.angle += 1;
     },
 
     jump: function() {
@@ -52,6 +54,7 @@ var mainState = {
         this.bird.body.velocity.y = -350;
 
         // Jump animation
+        game.add.tween(this.bird).to({angle: -20}, 100).start();
 
     },
 
